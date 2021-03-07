20340a518af467aa66c44c66b862bc14cb55f5a9
kuni10saito <kuni.saito@gmail.com>
2021-03-07 12:52:17 +0900

    Add files via upload

---
+++ dataload.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+os.system("pip install japanize_matplotlib")
+import japanize_matplotlib
+
+
+table=pd.read_csv('data_circle.csv')
+label=table["エネルギー種別"].values
+data=table["消費量"].values
+
+
+table=pd.read_csv('data_line.csv')
+label=table["年次"].values
+data1=table["出生数"].values
+data2=table["合計特殊出生率"].values
+
